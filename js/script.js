ad0a4f0553a9622da416a720c494b4da42c09969
vjova <vicki.jovan@gmail.com>
2022-08-24 07:37:04 -0700

    Quote Array
    Added array "quotes" with 6 quotes and sources

---
+++ js/script.js
@@ -0,0 +1,67 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+  quote:`“Be kind whenever possible. It is always possible."`,
+  source: "14th Dalai Lama",
+  citation: ``,
+  year: ``,
+  },
+  {
+  quote: `“Carry out a random act of kindness, with no expectation of reward, safe in the knowledge that one day someone might do the same for you.”`,
+  source: `Princess Diana`,
+  citation: ``,
+  year: ``,
+  },
+  {quote: `“What wisdom can you find that is greater than kindness?”`,
+  source: `Jean-Jacques Rousseau`,
+  citation: ``,
+  year: ``,
+  },
+  {quote: `A kind word is like a spring day`,
+  source: `Russian Proverb`,
+  citation: ``,
+  year: ``,
+  },
+  {quote: `“Do your little bit of good where you are; it’s those little bits of good put together that overwhelm the world.” `,
+  source: `Desmond Tutu`,
+  citation: ``,
+  year: ``,
+  }
+  {
+  quote: `“Unexpected kindness is the most powerful, least costly, and most underrated agent of human change.”`,
+  source: `Bob Kerrey`,
+  citation: ``,
+  year: ``,
+  }
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
